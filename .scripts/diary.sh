8deaf25091d6ff55b33f76a17108c88ed9cf0210
summis <summanentommi@gmail.com>
2020-08-30 12:09:03 +0300

    Use Goyo plugin

--- .scripts/diary.sh
+++ .scripts/diary.sh
@@ -3,9 +3,9 @@
 # Otherwise just opens file with text editor
 FILE=~/documents/diary/$(date +%Y%m%d.txt)
 
-[ ! -f "$FILE" ] && echo $(date +%A)$'\n'$(date +'%Y-%m-%d klo %H:%M')$'\n\n' >> $FILE 
+[ ! -f "$FILE" ] && echo $(date +%A)$'\n'$(date +'%Y-%m-%d klo %H:%M')$'\n\n' >> $FILE
 
 # open terminal for editor
-kitty vim + $FILE > /dev/null
+kitty vim -c Goyo + $FILE > /dev/null
 
 exit 0
